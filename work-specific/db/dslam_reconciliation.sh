8477f68e6972d80ef4edf79655e86a1ca619d531
Pablo Ganuza Vidal <pabloganuza@gmail.com>
2019-12-20 12:43:45 +0100

    Fix number of fields in request. Ports were being parsed as comments and snmp_port was being assigned a default value.

--- work-specific/db/dslam_reconciliation.sh
+++ work-specific/db/dslam_reconciliation.sh
@@ -1,4 +1,4 @@
 sqlturbo.py -u $DB -n -f csv -s ',' "
-	select 'RECON', dslam, DSLAM_IP, DSLAM_TYPE, INTERFACE_TYPE, NMS_NAME, SNMP_COMMUNITY_RO, SNMP_COMMUNITY_RW, SNMP_PORT 
+	select 'RECON', dslam, DSLAM_IP, DSLAM_TYPE, INTERFACE_TYPE, NMS_NAME, SNMP_COMMUNITY_RO, SNMP_COMMUNITY_RW, null, SNMP_PORT 
 		from v_dslams 
 		where status=0;"
