3c10745dfccab8c71770ed62abb0cfd9d8d758be
Pablo Ganuza Vidal <pabloganuza@gmail.com>
2019-12-20 17:33:05 +0100

    sltp piece is written in a table now.

---
+++ work-specific/reports/release-report/sltp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys, re
+
+f_sltp_toc = sys.argv[1]
+
+#print('###############################\n')
+with open(f_sltp_toc, mode='r') as infile:
+    for line in infile:
+#        print('-------------------------------\n')
+        line = line.strip()
+#        print('Parsing \'' + line + '\'')
+        if not line:
+#            print('Empty line!!')
+            continue
+
+        if re.search('^[0-9]\s', line):
+            x = re.findall('^([0-9])\s+(.*)', line)
+            print("### %s" % (x[0][1]))
+            print('| ID | Title | Status |')
+            print('| -- | ----- | ------ |')
+        if re.search('^[0-9]\.', line):
+            title = re.findall('^([0-9])\.([0-9]+)\s+(.*)', line)
+            status = re.findall('^Status:\s*(.*)', infile.readline())
+            if status:
+                print("| %s.%s | %s | %s |" % (title[0][0], title[0][1], title[0][2], status[0]))
+
+#print('bam!')
