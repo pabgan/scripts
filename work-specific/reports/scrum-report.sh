19bd2e35c9678c410a8b5367bda15ba40263738a
Pablo Ganuza Vidal <pabloganuza@gmail.com>
2020-01-07 15:39:25 +0100

    Move check for commits to the end, because it takes forever.

--- work-specific/reports/scrum-report.sh
+++ work-specific/reports/scrum-report.sh
@@ -13,17 +13,18 @@ for d ({$days_to_report..0}) do
 	grep -E "^x $(date --iso-8601 --date="$d days ago")" ~/.todo-txt/trabajo-done.txt ~/.todo-txt/trabajo-todo.txt | grep -Ev $ommited_tags | sed 's/.*:x //g'
 done
 
+echo "\n================= OTHER THINGS DONE ================="
+jrnl -from "\"$days_to_report days ago at 15:50\""
+
+echo "\n================== THINGS TO TELL ==================="
+todo.sh ls @scrum | head -n -2
+
 echo "\n===================== COMMITS ======================="
 # Execute a command from another directory [1]
 ( cd ~/Workspace/expresse/dsloExpresse/ ; cvs log -d \>"$days_to_report days ago" 2>/dev/null | grep "author: pganuza" -A1 | grep -e "\(DSLE\|QA\)-" | sort| uniq )
 ( cd ~/Workspace/expresse/dsloExpresseAdditional/ ; cvs log -d \>"$days_to_report days ago" 2>/dev/null | grep "author: pganuza" -A1 | grep -e "\(DSLE\|QA\)-" | sort| uniq )
 ( cd ~/Workspace/expresse/GuiWebServiceProject/ ; cvs log -d \>"$days_to_report days ago" 2>/dev/null | grep "author: pganuza" -A1 | grep -e "\(DSLE\|QA\)-" | sort| uniq )
 
-echo "\n================= OTHER THINGS DONE ================="
-jrnl -from "\"$days_to_report days ago at 15:50\""
-
-echo "\n================== THINGS TO TELL ==================="
-todo.sh ls @scrum | head -n -2
 
 ###################################################33
 # REFERENCES
