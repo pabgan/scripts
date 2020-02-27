f5724f6fba4babd9da63e3be3cbdbe6807b198e3
Pablo Ganuza Vidal <pabloganuza@gmail.com>
2020-02-27 16:45:56 +0100

    Manage input parameters.

--- work-specific/db/line_reconciliation.sh
+++ work-specific/db/line_reconciliation.sh
@@ -1,3 +1,15 @@
+#!/bin/zsh
+if [[ "$1" == "-h" ]]; then
+	echo "Usage: `basename $0` [db_schema]"
+	echo "       If no schema name is provided, the value of \$CUSTOMER_DB will be used."
+	exit 0
+fi
+if [ -z $1];
+then
+	DB=$CUSTOMER_DB
+else
+	DB=$1
+fi
 sqlturbo.py -u $DB -f csv -s ',' -n "
 	select 'RECON', line_id, dslam, port, service_product, circuit_id, PO_ENABLED, PE_ENABLED, NETWORK, TECHNOLOGY 
 		from v_ports where status=0 and IS_BONDED=0 
