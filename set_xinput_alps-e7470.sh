0e41c974c8fb65398b1a4e4d64da9ccb8514172a
Pablo Ganuza Vidal <pabloganuza@gmail.com>
2020-03-10 16:49:44 +0100

    A litlle bit more speed.

--- set_xinput_alps-e7470.sh
+++ set_xinput_alps-e7470.sh
@@ -1,3 +1,4 @@
 #/bin/bash
-xinput set-prop 'AlpsPS/2 ALPS DualPoint Stick' 'libinput Accel Profile Enabled' 0, 1
-xinput set-float-prop 'AlpsPS/2 ALPS DualPoint Stick' 'libinput Accel Speed' -0.8
+#xinput set-prop 'AlpsPS/2 ALPS DualPoint Stick' 'libinput Accel Profile Enabled' 0, 1
+xinput set-prop 'AlpsPS/2 ALPS DualPoint Stick' 'libinput Accel Profile Enabled' 0, 0
+xinput set-float-prop 'AlpsPS/2 ALPS DualPoint Stick' 'libinput Accel Speed' -0.6
