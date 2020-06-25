dc11f6d163cb6ee877529a0ea67898c85db197cd
Pablo Ganuza Vidal <pabloganuza@gmail.com>
2020-06-25 09:51:21 +0200

    Adjust sensitivity.

--- set_xinput_alps-e7470.sh
+++ set_xinput_alps-e7470.sh
@@ -1,4 +1,8 @@
 #/bin/bash
-#xinput set-prop 'AlpsPS/2 ALPS DualPoint Stick' 'libinput Accel Profile Enabled' 0, 1
-xinput set-prop 'AlpsPS/2 ALPS DualPoint Stick' 'libinput Accel Profile Enabled' 0, 0
-xinput set-float-prop 'AlpsPS/2 ALPS DualPoint Stick' 'libinput Accel Speed' -0.6
+xinput set-prop 'AlpsPS/2 ALPS DualPoint Stick' 'libinput Accel Profile Enabled' 0, 1
+#xinput set-prop 'AlpsPS/2 ALPS DualPoint Stick' 'libinput Accel Profile Enabled' 0, 0
+#xinput set-float-prop 'AlpsPS/2 ALPS DualPoint Stick' 'Device Accel Constant Deceleration' 0
+#xinput set-float-prop 'AlpsPS/2 ALPS DualPoint Stick' 'libinput Accel Speed' -0.6
+xinput set-float-prop  'AlpsPS/2 ALPS DualPoint Stick'      'libinput Accel Speed'      -0.70
+#xinput set-float-prop 'AlpsPS/2 ALPS DualPoint Stick' 'libinput Accel Speed Default'    0.000000
+#xinput set-pro libinput Accel Profile Enabled Default (332):     1, 0
