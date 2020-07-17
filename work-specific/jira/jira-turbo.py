b38fadbc643a96d8a0d22b91c36a9e3b2f481f99
Pablo Ganuza Vidal <pabloganuza@gmail.com>
2020-07-17 11:03:41 +0200

    Use argparse to properly allow to search for issues and print their summary.

--- work-specific/jira/jira-turbo.py
+++ work-specific/jira/jira-turbo.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
+import argparse
 from jira import JIRA
 
 jira = JIRA('https://assia-inc.atlassian.net/')
 
-issue=jira.issue('DSLE-21230')
-print(issue.fields.summary)
+
+if __name__ == "__main__":
+    desc= "Interact with Jira instance"
+    desc+="Following ASSIA-QA-land structure, it only takes one parameter by command line (the path to the config.properties file) where all the others setting should be there.\n"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("action", help="hello")
+    parser.add_argument("--issue", required=False)
+    parser.add_argument("--summary", action='store_true', required=False)
+    args = parser.parse_args()
+
+    if args.action == 'query':
+        if args.summary:
+            issue=jira.issue(args.issue)
+            print(issue.fields.summary)
+        else:
+            print('Querying')
+
