d208ac958ca7e109710e207561a43a9d5ab0bcec
Pablo Ganuza Vidal <pabloganuza@gmail.com>
2020-10-29 14:50:00 +0100

    Fix commit report not showing everything and other things.

--- work-specific/reports/scrum-report.sh
+++ work-specific/reports/scrum-report.sh
@@ -13,20 +13,20 @@ echo "Scrum report since day: $(date --date="$1 days ago")"
 echo "\n================== COMPLETED TASKS =================="
 ommited_tags='@scrum|@qa-meeting'
 for d ({$days_to_report..0}) do
-	grep -E "^x $(date --iso-8601 --date="$d days ago")" ~/Documentos/organizacion/todo/done.txt ~/Documentos/organizacion/todo/todo.txt | grep -Ev $ommited_tags | sed 's/.*:x //g'
+	grep -E "^x $(date --iso-8601 --date="$d days ago")" ~/Documents/organizacion/todo/done.txt ~/Documents/organizacion/todo/todo.txt | grep -Ev $ommited_tags | sed 's/.*:x //g'
 done
 
 echo "\n================= OTHER THINGS DONE ================="
 jrnl -from "\"$days_to_report days ago at 15:50\""
 
-echo "\n================== THINGS TO TELL ==================="
-todo-txt ls @scrum | head -n -2
+echo "================== THINGS TO TELL ==================="
+todo-txt -d $XDG_CONFIG_HOME/todo/config ls @scrum | head -n -2
 
 echo "\n===================== COMMITS ======================="
 # Execute a command from another directory [1]
-( cd ~/Repositorios/ASSIA/expresse/dsloExpresse/ ; cvs log -d \>"$days_to_report days ago" 2>/dev/null | grep "author: pganuza" -A1 | tail -n1 )
-( cd ~/Repositorios/ASSIA/expresse/dsloExpresseAdditional/ ; cvs log -d \>"$days_to_report days ago" 2>/dev/null | grep "author: pganuza" -A1 | tail -n1 )
-( cd ~/Repositorios/ASSIA/expresse/GuiWebServiceProject/ ; cvs log -d \>"$days_to_report days ago" 2>/dev/null | grep "author: pganuza" -A1 | tail -n1 )
+( cd ~/Repositorios/ASSIA/expresse/dsloExpresse/ ; cvs log -d \>"$days_to_report days ago" 2>/dev/null | grep "author: pganuza" -A1 | sed '/^date/d' | sed '/--/d' | sort | uniq )
+( cd ~/Repositorios/ASSIA/expresse/dsloExpresseAdditional/ ; cvs log -d \>"$days_to_report days ago" 2>/dev/null | grep "author: pganuza" -A1 | sed '/^date/d' | sed '/--/d' | sort | uniq )
+( cd ~/Repositorios/ASSIA/expresse/GuiWebServiceProject/ ; cvs log -d \>"$days_to_report days ago" 2>/dev/null | grep "author: pganuza" -A1 | sed '/^date/d' | sed '/--/d' | sort | uniq )
 
 
 ###################################################33
