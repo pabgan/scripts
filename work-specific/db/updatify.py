d8e1b7738202d0bad320d5221a0c4f47cdda580f
Pablo Ganuza Vidal <pabloganuza@gmail.com>
2020-11-20 10:29:37 +0100

    Do not try to be smart about value quotes, so `sysdate` is not quoted and we do not break everything.

--- work-specific/db/updatify.py
+++ work-specific/db/updatify.py
@@ -2,6 +2,8 @@
 # coding: utf-8
 import re, sys
 
+####################################
+# Auxiliary functions
 def parse_insert(line):
     line_parsed = re.findall('INSERT\s+INTO\s+(.+)\s*\((.+)\)\s*VALUES\s*\((.+)\)\s*;', line, re.IGNORECASE)
     result = {}
@@ -10,13 +12,14 @@ def parse_insert(line):
         #print(line_parsed)
         result["TABLE"] = line_parsed[0][0].strip()
         keys = line_parsed[0][1].split(",")
-        values = [value.replace("'","") for value in line_parsed[0][2].split(",")]
+        values = [value for value in line_parsed[0][2].split(",")]
         for (key, value) in zip(keys, values):
             result[key.strip().upper()] = value.strip()
 
     return result
 
-
+####################################
+# 0. Handle call
 # We go through the provisioning file
 if len(sys.argv) < 2:
     fprovname = raw_input("Enter provisioning file name: ")
@@ -24,10 +27,8 @@ else:
     fprovname = sys.argv[1]
 fprov = open(fprovname)
 
-# We create the output file
-#foutput = open(fprovname + ".updatified", "w")
-
-# Now we parse every line in the provisioning file
+####################################
+# 1. Now we parse every line in the provisioning file
 for line in fprov:
     values = parse_insert(line)
     
@@ -41,11 +42,11 @@ for line in fprov:
                 continue
 
             if first:
-                update_sentence = update_sentence + k + '=\'' + v + '\''
+                update_sentence = update_sentence + k + '=' + v
                 first = False
             else:
-                update_sentence = update_sentence + ', ' + k + '=\'' + v + '\''
+                update_sentence = update_sentence + ', ' + k + '=' + v
         
-        update_sentence = update_sentence + ' WHERE LINE_ID=\'' + values.get('LINE_ID') + '\';'
+        update_sentence = update_sentence + ' WHERE LINE_ID=' + values.get('LINE_ID') + ';'
 
         print(update_sentence)
