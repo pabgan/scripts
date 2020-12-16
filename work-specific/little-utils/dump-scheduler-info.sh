bb328bbf9dea074c425a6d5590740d28c3420c58
Pablo Ganuza Vidal <pabloganuza@gmail.com>
2020-12-16 08:19:07 +0100

    Print password to stderr.

--- work-specific/little-utils/dump-scheduler-info.sh
+++ work-specific/little-utils/dump-scheduler-info.sh
@@ -7,7 +7,7 @@ fi
 host=$1
 ssh user@$host '
 password=$(grep -E "^monitorRole" /home/user/install/server/config/assia.jmxremote.password | cut -d" " -f 2)
-echo "Using \"$password\" as password."
+echo "Using \"$password\" as password." 1>&2
 cd /home/user/install/server
 export JAVA_HOME=/usr/local/java/jdk
 ant -f ant/scheduler_build.xml dumpSchedulerInfo -Dparameters="localhost:10365 $password"
