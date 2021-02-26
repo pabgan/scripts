6b96edced5f888af6a42591a32dbb2ffa66e20ce
Pablo Ganuza Vidal <pganuza@assia-inc.com>
2021-02-26 16:45:27 +0100

    Better organization.

--- work-specific/provisioning/get_current_sps.sh
+++ work-specific/provisioning/get_current_sps.sh
@@ -1,2 +1,3 @@
-for DSLAM in Randburg_MSAN_PON Cotswold_ISAM_PON
+for DSLAM in Randburg_MSAN_PON Cotswold_ISAM_PON; do
 	        grep 'PORT_INFO' simulators_provisioning_info.sql | grep $DSLAM | cut -d',' -f16 | sort | uniq | sed "s/'//g" > $DSLAM-current_sps.txt
+done
