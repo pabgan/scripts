0d11fac13a14e2175f15b14c126b3b55865de262
Pablo Ganuza Vidal <pganuza@assia-inc.com>
2021-03-03 16:55:39 +0100

    Lots of changes. Still trying to figure out how to organize this.

--- work-specific/customer-release-testing/test-release.sh
+++ work-specific/customer-release-testing/test-release.sh
@@ -1,14 +1,33 @@
 #!/bin/zsh
 source ~/.zshrc
 
-get_deliverables(){
+#######################
+# 0. SET UP
+get_deliverables() {
 	mkdir -p deliverables
 	pushd deliverables
 	wget_sqls
 	popd
-	#TODO: Descargar NCD
-	#TODO: aplicarle docx2txt
+}
+#TODO: Descargar NCD
+#TODO: aplicarle docx2txt
+change_percentiles() {
+	ssh user@$CUSTOMER_ENV.assia-inc.com "
+		sed -i 's/pon.pe.estimator.throughput.stats.min.num.percentiles.*=.*/pon.pe.estimator.throughput.stats.min.num.percentiles = 2/' ~/install/server/config/pon.pe.estimator.properties ;;
+		sed -i 's/pon.sr.throughput.stats.min.num.percentiles.*=.*/pon.sr.throughput.stats.min.num.percentiles = 2/' ~/install/server/config/pon.sr.properties ;;
+		ant -f ~/install/server/build.xml pe restart
+		"
 }
 
-get_deliverables
+#######################
+# 1. TEST
+#
+#TODO: diff?
 #TODO: Descargar nuevo documento SLTP
+#TODO: whitelist para OI/Colombia
+#TODO: diff NCD
+#TODO: diff SLTP
+
+#######################
+# 2. Tear down
+#TODO: Cerrar release
