d99f30fe7405d85a1c68a1347372297bf2726629
Pablo Ganuza Vidal <pganuza@assia-inc.com>
2021-04-07 09:34:28 +0200

    change_percentiles() was failing with a syntax error.

--- work-specific/customer-release-testing/test-release.sh
+++ work-specific/customer-release-testing/test-release.sh
@@ -20,8 +20,8 @@ get_sltp() {
 #TODO: aplicarle docx2txt
 change_percentiles() {
 	ssh user@$CUSTOMER_ENV.assia-inc.com "
-		sed -i 's/pon.pe.estimator.throughput.stats.min.num.percentiles.*=.*/pon.pe.estimator.throughput.stats.min.num.percentiles = 2/' ~/install/server/config/pon.pe.estimator.properties ;;
-		sed -i 's/pon.sr.throughput.stats.min.num.percentiles.*=.*/pon.sr.throughput.stats.min.num.percentiles = 2/' ~/install/server/config/pon.sr.properties ;;
+		sed -i 's/pon.pe.estimator.throughput.stats.min.num.percentiles.*=.*/pon.pe.estimator.throughput.stats.min.num.percentiles = 2/' ~/install/server/config/pon.pe.estimator.properties
+		sed -i 's/pon.sr.throughput.stats.min.num.percentiles.*=.*/pon.sr.throughput.stats.min.num.percentiles = 2/' ~/install/server/config/pon.sr.properties
 		ant -f ~/install/server/build.xml pe restart
 		"
 }
