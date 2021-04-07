58c9dec7e9c1245cfe54cde297656baec15558f7
Pablo Ganuza Vidal <pganuza@assia-inc.com>
2021-04-07 10:05:14 +0200

    Gracefully handle the case in which the macro to populate tests results has not been executed yet. In that case the format is different and the parsing fails. We print now an informative message then.

--- work-specific/customer-release-testing/status/sltp.py
+++ work-specific/customer-release-testing/status/sltp.py
@@ -9,24 +9,27 @@ def print_table_header():
 
 #print('###############################\n')
 with open(f_sltp_toc, mode='r') as infile:
-    for line in infile:
-#        print('-------------------------------\n')
-        line = line.strip()
-#        print('Parsing \'' + line + '\'')
-        if not line:
-#            print('Empty line!!')
-            continue
+    try:
+        for line in infile:
+    #        print('-------------------------------\n')
+            line = line.strip()
+    #        print('Parsing \'' + line + '\'')
+            if not line:
+    #            print('Empty line!!')
+                continue
 
-        # Generic / Customer specific division
-        if re.search('^[0-9]\s', line):
-            x = re.findall('^([0-9])\s+(.*)', line)
-            print("### %s" % (x[0][1]))
-            print_table_header()
-        # Actual list of tests
-        if re.search('^[0-9]\.', line):
-            title = re.findall('^([0-9])\.([0-9]+)\s+(.*)', line)
-            status = re.findall('^Status:\s*(.*)', infile.readline())
-            if status:
-                print("| %s.%s | %s | %s |" % (title[0][0], title[0][1], title[0][2], status[0]))
+            # Generic / Customer specific division
+            if re.search('^[0-9]\s', line):
+                x = re.findall('^([0-9])\s+(.*)', line)
+                print("### %s" % (x[0][1]))
+                print_table_header()
+            # Actual list of tests
+            if re.search('^[0-9]\.', line):
+                title = re.findall('^([0-9])\.([0-9]+)\s+(.*)', line)
+                status = re.findall('^Status:\s*(.*)', infile.readline())
+                if status:
+                    print("| %s.%s | %s | %s |" % (title[0][0], title[0][1], title[0][2], status[0]))
+    except Exception:
+        print("Not started yet.")
 
-#print('bam!')
+    #print('bam!')
