ce5ac801e5f35aa09b45e661bd69ba736567c744
Pablo Ganuza Vidal <pganuza@assia-inc.com>
2021-06-18 16:34:16 +0200

    Execute request against $CUSTOMER_ENV if no parameter is provided. Now there is no managing of parameters.

--- work-specific/little-utils/dump-scheduler-info.sh
+++ work-specific/little-utils/dump-scheduler-info.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ $# != 1 ]; then
-	echo 1>&2 "*** Usage: $0 [host]"
-	exit 2
-fi
+#if [ $# != 1 ]; then
+#	echo 1>&2 "*** Usage: $0 [host]"
+#	exit 2
+#fi
 
-host=$1
+host="${1:-$CUSTOMER_ENV}"
 ssh user@$host '
 password=$(grep -E "^monitorRole" /home/user/install/server/config/assia.jmxremote.password | cut -d" " -f 2)
 echo "Using \"$password\" as password." 1>&2
